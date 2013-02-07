502317b5d3cef52ca9d30b44e715a05051ac223a
Dirk Hesse <herr.dirk.hesse@gmail.com>
2013-02-07 19:48:21 +0000

    Added analytic result for E_0^m and fixed a bug.
    There was a square in E_0^s that did not belong there.

--- analytic/E.py
+++ analytic/E.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 from math import pi
+import matplotlib.pyplot as plt
 
 N = 3
 
@@ -33,12 +34,36 @@ def tsqEs(L, T, x, c):
             num = 0
             for i in (1,2,3):
                 num += prsq * np.cos(p[i] / 2)**2 \
-                        - (ring(p[i]) * np.cos(p[i] / 2)**2)**2
+                        - (ring(p[i]) * np.cos(p[i] / 2))**2
             num *= shat(p[0], x)**2
             if psqd:
                 result += tmp * num / psqd
     rho = float(T) / L
     return (N**2 - 1) * c**4 * result / 128 / rho
 
+def tsqEm(L, T, x, c):
+    n = [0,0,0,0]
+    result = 0
+    for n[0] in range(-T, T):
+      for n[1] in range(-L/2, L/2):
+        for n[2] in range(-L/2, L/2):
+          for n[3] in range(-L/2, L/2):
+            p = np.array([ni * 2 * pi / LL for ni, LL in zip(n, (T, L, L, L))])
+            psqd = np.sum((hat(i)**2 for i in p[1:])) + check(p[0])**2
+            tmp = np.exp(- L**2 * c**2 / 4 * psqd)
+            prsq = np.sum((ring(i)**2 for i in p[1:]))
+            num = prsq *  np.cos(p[0] / 4)**2
+            for i in (1,2,3):
+                num += 0.25 * (hat(p[0]) * np.cos(p[i] / 2))**2
+            num *= chat(p[0], x - 0.5)**2
+            if psqd:
+                 result += tmp * num / psqd
+    rho = float(T) / L
+    return (N**2 - 1) * c**4 * result / 128 / rho
+
 if __name__ == "__main__":
-    print tsqEs(4, 4, 2, 0.5)
+    for f in (tsqEs, tsqEm):
+        for c in [np.sqrt(t / 4**2 * 8) for t in (.18, .36, .54)]:
+            print f(8,8,4,c),
+        print
+            #        print c, tsqEs(4, 4, 2, c), tsqEm(4, 4, 2, c)
