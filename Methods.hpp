f9bc2158ecb90e2fb9894f6a348b559246ab28a9
Dirk Hesse <herr.dirk.hesse@gmail.com>
2013-02-21 10:48:41 +0000

    There was a bug in pretty much all of the methods.
    I tried to figure out the time extent by using
    
      int T = U.extent(0);
    
    However, due to the SF boundary conditions this should be
    
      int T = U.extent(0) - 1;
    
    since we have in LocalQuench etc.
    
      // for SF boundary: set the time extend to T + 1
      e[0] = T + 1;

--- Methods.hpp
+++ Methods.hpp
@@ -38,7 +38,7 @@ namespace meth{
     void sf_gauge_fixing(Fld_t& U, const double& alpha){
       typedef kernels::GaugeFixingKernel<GF_MODE, Fld_t> GaugeFixingKernel;
       detail::bnd_gauge_fixing(U, alpha, typename kernels::std_types<Fld_t>::bgf_t());
-      int T = U.extent(0);
+      int T = U.extent(0) - 1;
       GaugeFixingKernel gf(alpha);
       for (int t = 1; t < T; ++t)
 	U.apply_on_timeslice(gf, t);
@@ -51,7 +51,7 @@ namespace meth{
     typedef typename kernels::WF_RK_3<Fld_t, kernels::StapleSqKernel<Fld_t> > wf3_t;
     typedef typename kernels::std_types<Fld_t>::direction_t Direction;
     Fld_t F(U);
-    int T = U.extent(0);
+    int T = U.extent(0) - 1;
     std::vector<wf1_t> wf1;
     for (Direction mu; mu.is_good(); ++mu)
       wf1.push_back(wf1_t(mu, eps, F));
@@ -81,7 +81,7 @@ namespace meth{
     typedef typename kernels::WF_RK2_2<Fld_t, kernels::StapleSqKernel<Fld_t> > wf2_t;
     typedef typename kernels::std_types<Fld_t>::direction_t Direction;
     Fld_t F(U);
-    int T = U.extent(0);
+    int T = U.extent(0) - 1;
     std::vector<wf1_t> wf1;
     for (Direction mu; mu.is_good(); ++mu)
       wf1.push_back(wf1_t(mu, eps, F));
@@ -106,7 +106,7 @@ namespace meth{
       typedef typename kernels::GU_RK_3<Fld_t, kernels::StapleSqKernel<Fld_t>, RK_t > wf3_t;
       typedef typename kernels::std_types<Fld_t>::direction_t Direction;
       Fld_t F(U);
-      int T = U.extent(0);
+      int T = U.extent(0) - 1;
       std::vector<wf1_t> wf1;
       for (Direction mu; mu.is_good(); ++mu)
 	wf1.push_back(wf1_t(mu, eps, F, R[mu]));
@@ -135,7 +135,7 @@ namespace meth{
       typedef typename kernels::GU_RK2_2<Fld_t, kernels::StapleSqKernel<Fld_t>, RK_t > wf2_t;
       typedef typename kernels::std_types<Fld_t>::direction_t Direction;
       Fld_t F(U);
-      int T = U.extent(0);
+      int T = U.extent(0) - 1;
       std::vector<wf1_t> wf1;
       for (Direction mu; mu.is_good(); ++mu)
 	wf1.push_back(wf1_t(mu, eps, F, R[mu]));
