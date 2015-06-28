c15c88fd8e3f282b42272563ad089ff8144c30e4
Dirk Hesse <herr.dirk.hesse@gmail.com>
2015-06-28 18:45:07 +0200

    Adding forgotten Kernel header.

---
+++ include/Kernels/generic/Project.hpp
@@ -0,0 +1,38 @@
+#ifndef PROJECT_H_
+#define PROJECT_H_
+
+#include <Kernels.hpp>
+#include <Background.h>
+
+namespace kernels {
+namespace proj {
+
+  template <class Fld_t>
+  struct ProjectSUN {
+    typedef typename kernels::std_types<Fld_t>::ptSU3_t ptSU3;
+    typedef typename kernels::std_types<Fld_t>::point_t Point;
+    typedef typename kernels::std_types<Fld_t>::direction_t Direction;
+    typedef typename kernels::std_types<Fld_t>::bgf_t BGF;
+    static const int ORD = kernels::std_types<Fld_t>::order;
+    static const int n_cb = 0;
+
+    ProjectSUN(const Direction& mu) : mu(mu) { }
+
+    void operator()(Fld_t& U, const Point& n) {
+      ptSU3 tmp;
+      BGF Vinv = U[n][mu].bgf().inverse();
+      BGF V = U[n][mu].bgf();
+      for (int i = 0; i < ORD; ++i)
+	tmp[i] = U[n][mu][i]*Vinv;
+      tmp = exp<BGF, ORD>(tmp.reH());
+      for (int i = 0; i < ORD; ++i)
+	U[n][mu][i] = tmp[i] * V;
+    }
+
+    Direction mu;
+  };
+  
+} 
+}
+
+#endif
